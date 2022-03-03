9116449b51ff6ce4e9ede90a04e508b215921db8
azeimbekakis <azeimbekakis@gmail.com>
2022-03-03 18:39:43 -0500

    Copula-based error in time series bootstrap

--- Code/tspb.R
+++ Code/tspb.R
@@ -1,16 +1,16 @@
-n <- 100
-x <- arima.sim(list(ar = 0.4), n)
+library(copula)
+library(dgof)
 
 ks.ts.pb <- function(x, dist, B = 1000) {
     ## example assuming normal distribution; normal copula
     ## get observed stat
     n <- length(x)
-    theta <- fit(x) # theta = (mu, sigma)
-    stat  <- ks.test(x, dist, theta)
+    stat  <- ks.test(x, dist)$statistic
     ## get lag-1 sample auto-spearman rho
     rho  <-  cor(x[-1], x[-n], method = "spearman")
     r <- iRho(normalCopula(), rho)
     ## parametric bootstrap to get an empirical distribution of stat
+    stat.b <- double(n)
     ## set up containers
     u <- double(n)
     for (i in 1:B) {
@@ -20,12 +20,22 @@ ks.ts.pb <- function(x, dist, B = 1000) {
         u <- runif(1)
         for (j in 2:n) {
             ## could test other copulas later
-            u[j]  <- copula::cCopula(u[j - 1], normalCopula(r))
+            u[j]  <- cCopula(u[j - 1], normalCopula(r))
         }
-        x.b <- qnorm(u, mean = theta[1], sd = theta[2])
+        x.b <- qnorm(u)
         ## fit it
-        theta.b <- fit(x.b)
-        stat.b <- ks.test(x.b, dist, theta.b)
+        mu.b <- mean(x.b)
+        sd.b <- sd(x.b)
+        stat.b[i] <- ks.test(x.b, dist, mu.b, sd.b)$statistic
     }
+    
+    p.value <- (sum(stat.b >= stat) + 0.5) / (B + 1)
+    
+    return(p.value)
     # return empirical p-value    
 }
+
+n <- 100
+ar = 0.5
+x <- arima.sim(list(ar = ar), rand.gen = rnorm, sd = sqrt(1-ar^2), n = n)
+ks.ts.pb(x, "pnorm")
